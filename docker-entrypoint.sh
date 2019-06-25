a1b41c26cd218bee8f5debd5fe7312ce21000709
ChristianKahmann <christiangoldlauter@googlemail.com>
2019-06-25 18:41:04 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 #su - solr -c "/opt/solr/bin/solr start -m 2g"
 #[ -d /var/solr/data/ ] && su - solr -c "/opt/solr/bin/solr create_core -c iLCM -p 8983 -d /store/solr/config/iLCM"
 #echo christian | sudo -S /usr/bin/mysqld_safe --basedir=/usr & sleep 5s
+/home/christian/expectfile.sh
 echo "database started"
 #echo christian | sudo -S su - solr -c "/opt/solr/bin/solr start -m 2g"
 #echo christian | sudo -S su - solr -c "/opt/solr/bin/solr create_core -c iLCM -p 8983 -d /store/solr/config/iLCM"
