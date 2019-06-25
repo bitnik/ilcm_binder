1fdbbf4341627471e0ea01c91b0c32678211e470
ChristianKahmann <christiangoldlauter@googlemail.com>
2019-06-25 20:28:22 +0200

    Update expectfile.sh

--- expectfile.sh
+++ expectfile.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/expect
 set pssword [lrange $argv 0 0]
 spawn su docker -c "echo docker | sudo -S service mysql start & sleep 2s"
-#spawn su docker -c "sh /home/christian/start_services.sh"
 expect "Password:" { send "$pssword\r" }
 interact
 exit
