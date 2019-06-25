7e49421c4f31eaeaea519d2aa2fb99c4a3b2c8e2
ChristianKahmann <christiangoldlauter@googlemail.com>
2019-06-25 20:04:34 +0200

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo christian | sudo -S -u solr /opt/solr/bin/solr start -m 2g
+echo docker | sudo -S -u solr /opt/solr/bin/solr start -m 2g
 
-echo christian | sudo -S -u solr /opt/solr/bin/solr create_core -c iLCM -p 8983 -d /store/solr/config/iLCM
+echo docker | sudo -S -u solr /opt/solr/bin/solr create_core -c iLCM -p 8983 -d /store/solr/config/iLCM
 
-echo christian | sudo -S service mysql start
+echo docker | sudo -S service mysql start
