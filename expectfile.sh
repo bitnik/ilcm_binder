429d7b2abe42d3a254bf7cd224ad20b4794d151f
ChristianKahmann <christiangoldlauter@googlemail.com>
2019-06-26 13:39:44 +0200

    Update expectfile.sh

--- expectfile.sh
+++ expectfile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 set pssword [lrange $argv 0 0]
-spawn su docker -c "sh //start_services.sh"
+spawn su docker -c "sh /start_services.sh"
 expect "Password:" { send "$pssword\r" }
 sleep 10
 interact
